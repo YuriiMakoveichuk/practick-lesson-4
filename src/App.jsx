9f4dd463688735b2331f304e64e69e036d6e5b8e
Olga Blud <olga.blud@gmail.com>
2024-09-01 15:53:48 +0300

    add slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Header } from './components';
 import { lazy, useEffect } from 'react';
-import { getUserInfo } from 'service/opencagedataApi';
+
+import { useDispatch } from 'react-redux';
+import { fetchBaseCurrency } from './redux/currency/operations';
+import { setBaseCurrency } from './redux/currency/currencySlice';
 
 const Home = lazy(() => import('pages/Home'));
 const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords }) =>
-      getUserInfo(coords),
-    );
-  }, []);
+    const success = ({ coords }) => {
+      dispatch(fetchBaseCurrency(coords));
+    };
+    const error = () => {
+      dispatch(setBaseCurrency('USD'));
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, [dispatch]);
   return (
     <Routes>
       <Route path="/" element={<Header />}>
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,17 @@ import { App } from './App.jsx';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
