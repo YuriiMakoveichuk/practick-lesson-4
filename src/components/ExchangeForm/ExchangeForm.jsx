d2618f5c0b888b07d337d775120334c79c171eff
Olga Blud <olga.blud@gmail.com>
2024-09-01 17:13:24 +0300

    add exchange api

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -1,14 +1,33 @@
 import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
+import { useDispatch } from 'react-redux';
+import { fetchExchangeCurrency } from '../../redux/currency/operations';
 
 export const ExchangeForm = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = event => {
+    event.preventDefault();
+    const { value } = event.target.elements.currency;
+    console.log(value);
+    const [amount, from, , to] = value.split(' ');
+    dispatch(fetchExchangeCurrency({ amount, from, to }));
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <button className={styles.button} type="submit">
         <RiExchangeDollarFill className={styles.icon} />
       </button>
 
-      <input title="Request format 15 USD in UAH" className={styles.input} />
+      <input
+        type="text"
+        pattern="^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$"
+        placeholder="15 USD in UAH"
+        title="Request format 15 USD in UAH"
+        className={styles.input}
+        name="currency"
+        required
+      />
     </form>
   );
 };
--- src/redux/currency/currencySlice.js
+++ src/redux/currency/currencySlice.js
@@ -1,8 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchBaseCurrency } from './operations';
+import { fetchBaseCurrency, fetchExchangeCurrency } from './operations';
 
 const initialState = {
   baseCurrency: '',
+  exchangeInfo: null,
+  error: null,
+  isLoading: false,
 };
 const currencySlice = createSlice({
   name: 'currency',
@@ -13,9 +16,23 @@ const currencySlice = createSlice({
     },
   },
   extraReducers: builder =>
-    builder.addCase(fetchBaseCurrency.fulfilled, (state, { payload }) => {
-      state.baseCurrency = payload;
-    }),
+    builder
+      .addCase(fetchBaseCurrency.fulfilled, (state, { payload }) => {
+        state.baseCurrency = payload;
+      })
+      .addCase(fetchExchangeCurrency.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchExchangeCurrency.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.exchangeInfo = payload;
+      })
+      .addCase(fetchExchangeCurrency.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        state.exchangeInfo = null;
+      }),
 });
 export const { setBaseCurrency } = currencySlice.actions;
 export const currencyReducer = currencySlice.reducer;
